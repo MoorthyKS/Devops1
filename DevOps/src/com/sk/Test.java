5efc82be37ba0c6d765b93e3d91a8a7b6eca3e10
BKB & Family <BKB & Family@Sk>
2019-09-29 12:15:51 +0530

    1

--- DevOps/src/com/sk/Test.java
+++ DevOps/src/com/sk/Test.java
@@ -2,7 +2,7 @@ package com.sk;
 
 public class Test {
 	public static void main (String a[]) {
-		System.out.println("The Devops with GutHub ");
+		System.out.println("The Devops with GutHub");
 	}
 
 }
